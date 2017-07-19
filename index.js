7c5898d2585167518ef9303ea66e3901eadfadb8
gnuheike@gmail.com <gnuheike@gmail.com>
2017-07-19 14:50:55 +0600

    gen table chhange

--- index.js
+++ index.js
@@ -2,41 +2,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const http = require('http');
+require('dotenv').config();
 http.createServer(function (req, res) {}).listen(process.env.PORT || 6000);
 
-/*
- * Hash - string to #nnnnnn color
- */
-var stringToColour = function (str) {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-        var value = (hash >> (i * 8)) & 0xFF;
-        colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
-};
-
 /**
- * On new message received - relay to all authorized servers
+ * On new message received - relay to all role
  */
 client.on('message', msg => {
-    var embed = new Discord.RichEmbed()
-            .setAuthor(msg.member.displayName, msg.member.user.displayAvatarURL)
-            .setDescription(msg.content)
-            .setColor(stringToColour(msg.member.displayName));
-    if (typeof msg.attachments.first() !== 'undefined') {
-        embed.setImage(msg.attachments.first().url);
+    if (msg.channel.name === 'announce' && msg.member.roles.has('199362164408975360')) {
+        msg.guild.roles.find("name", "Рассвет").members.forEach(function (member) {
+            member.sendMessage(msg.content);
+        });    
     }
-
-    client.guilds.forEach(function (guild) {
-        if (client.user.id !== msg.author.id && msg.author.bot == false && guild.id !== msg.guild.id && msg.channel.name == 'general') {
-            guild.channels.find('name', 'general').sendEmbed(embed);
-        }
-    });
 });
 
 client.login(process.env.BOT_TOKEN);
